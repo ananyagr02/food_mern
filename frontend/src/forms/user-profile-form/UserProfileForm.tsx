f08588785b31b706cd20c59fe8470e4c3d03ace0
ananyagr02 <115359890+ananyagr02@users.noreply.github.com>
2024-08-29 15:54:31 +0530

    Update UserProfileForm.tsx

--- frontend/src/forms/user-profile-form/UserProfileForm.tsx
+++ frontend/src/forms/user-profile-form/UserProfileForm.tsx
@@ -12,17 +12,18 @@ import {z} from "zod";
 const formSchema = z.object({
     email: z.string().optional(), // read only field
     name: z.string().min(3,"Name is required"),
-    password: z.string().min(8, 'Password must be at least 8 characters'), // Add password field with min length
-    confirmPassword: z.string().min(8, 'Confirm password must be at least 8 characters'), // Add confirm password field with min length
-    phoneNumber: z.string().min(10, 'Please enter a valid phone number'),
-    addressLine1: z.string().min(1,"Address Line 1 is required"),
-    city: z.string().min(1,"City is required"),
+    password: z.string().min(8, 'Password must be at least 8 characters').optional(), // Add password field with min length
+    // confirmPassword: z.string().min(8, 'Confirm password must be at least 8 characters').optional(), // Add confirm password field with min length
+    phoneNumber: z.string().min(10, 'Please enter a valid phone number').optional(),
+    addressLine1: z.string().min(1,"Address Line 1 is required").optional(),
+    city: z.string().min(1,"City is required").optional(),
     country: z.string().optional(),
 
-}).refine((data) => data.password === data.confirmPassword, {
-    path: ['confirmPassword'],
-    message: 'Passwords do not match',
-  }); // Ensure passwords match
+})
+// .refine((data) => data.password === data.confirmPassword, {
+//     path: ['confirmPassword'],
+//     message: 'Passwords do not match',
+//   }); // Ensure passwords match
 
 
 type UserFormData = z.infer<typeof formSchema>  // infer from zod framework the type of each field in form schema
@@ -34,13 +35,16 @@ type Props = {
 }
 
 const UserProfileForm = ({ onSave, isLoading, currentUser }: Props) => {  // destructuring these props
+    console.log("CURRRRRNETTT UZZER",currentUser)
     const form = useForm<UserFormData>({   // useForm hook from react hook form library and type of form is userFormData
         resolver: zodResolver(formSchema),  // handles validation
         defaultValues: currentUser,
     }
 );
 useEffect(() => {
+    
     form.reset(currentUser)
+    console.log("RESET KAR DIYA ", currentUser)
 }, [currentUser, form])
 
     return (
@@ -56,60 +60,70 @@ useEffect(() => {
 
                 <FormField control = {form.control} name="email" render = {(field) => (
                     <FormItem>
-                        <FormLabel>Email</FormLabel>
+                        <FormLabel htmlFor="email">Email</FormLabel>
                         <FormControl>
-                            <Input {...field} disabled className = "bg-white"/> {/*form is managed by react-hook-form so field object contains a bunch of info and properties sbout the input and sends it to Input to display it all */}
+                            <Input id="email" {...field} readOnly className = "bg-white"/> {/*form is managed by react-hook-form so field object contains a bunch of info and properties sbout the input and sends it to Input to display it all */}
                         </FormControl>
                     </FormItem>)}/>
                     
                 <FormField control = {form.control} name="name" render = {(field) => (
                     <FormItem>
-                        <FormLabel>Name</FormLabel>
+                        <FormLabel htmlFor="name" >Name</FormLabel>
                         <FormControl>
-                            <Input {...field}  className = "bg-white"/> {/*form is managed by react-hook-form so field object contains a bunch of info and properties sbout the input and sends it to Input to display it all */}
+                            <Input id="name" {...field}  className = "bg-white"/> {/*form is managed by react-hook-form so field object contains a bunch of info and properties sbout the input and sends it to Input to display it all */}
                         </FormControl>
                         <FormMessage/>
                     </FormItem>)}/>
                     <FormField control = {form.control} name="password" render = {(field) => (
                     <FormItem>
-                        <FormLabel>Password</FormLabel>
+                        <FormLabel htmlFor="password">Password</FormLabel>
                         <FormControl>
-                            <Input {...field}  className = "bg-white"/> {/*form is managed by react-hook-form so field object contains a bunch of info and properties sbout the input and sends it to Input to display it all */}
+                            <Input id="password" {...field}  type="password" className = "bg-white"/> {/*form is managed by react-hook-form so field object contains a bunch of info and properties sbout the input and sends it to Input to display it all */}
                         </FormControl>
                         <FormMessage/>
                     </FormItem>)}/>
-                    <FormField control = {form.control} name="confirmPassword" render = {(field) => (
+                    {/* <FormField control = {form.control} name="confirmPassword" render = {(field) => (
                     <FormItem>
-                        <FormLabel>Confirm Password</FormLabel>
+                        <FormLabel htmlFor="confirmPassword">Confirm Password</FormLabel>
                         <FormControl>
-                            <Input {...field}  className = "bg-white"/> {/*form is managed by react-hook-form so field object contains a bunch of info and properties sbout the input and sends it to Input to display it all */}
+                            <Input id="confirmPassword" {...field}  type="password" className = "bg-white"/> {/*form is managed by react-hook-form so field object contains a bunch of info and properties sbout the input and sends it to Input to display it all */}
+                        {/*</FormControl>
+                        <FormMessage/>
+                    </FormItem>)}/> 
+                    */}
+
+                    <FormField control = {form.control} name="phoneNumber" render = {(field) => (
+                    <FormItem>
+                        <FormLabel htmlFor="phoneNumber">Phone Number</FormLabel>
+                        <FormControl>
+                            <Input id="phoneNumber" {...field}  className = "bg-white"/> {/*form is managed by react-hook-form so field object contains a bunch of info and properties sbout the input and sends it to Input to display it all */}
                         </FormControl>
                         <FormMessage/>
                     </FormItem>)}/>
                     <div className="flex flex-col md:flex-row gap-4">
                     <FormField control = {form.control} name="addressLine1" render = {(field) => (
                     <FormItem className="flex-1">
-                        <FormLabel>Address Line 1</FormLabel>
+                        <FormLabel htmlFor="addressLine1">Address Line 1</FormLabel>
                         <FormControl>
-                            <Input {...field}  className = "bg-white"/> {/*form is managed by react-hook-form so field object contains a bunch of info and properties sbout the input and sends it to Input to display it all */}
+                            <Input id="addressLine1" {...field}  className = "bg-white"/> {/*form is managed by react-hook-form so field object contains a bunch of info and properties sbout the input and sends it to Input to display it all */}
                         </FormControl>
                         <FormMessage/>
                     </FormItem>)}/>
 
                     <FormField control = {form.control} name="city" render = {(field) => (
                     <FormItem className="flex-1">
-                        <FormLabel>City</FormLabel>
+                        <FormLabel htmlFor="city">City</FormLabel>
                         <FormControl>
-                            <Input {...field}  className = "bg-white"/> {/*form is managed by react-hook-form so field object contains a bunch of info and properties sbout the input and sends it to Input to display it all */}
+                            <Input id="city" {...field}  className = "bg-white"/> {/*form is managed by react-hook-form so field object contains a bunch of info and properties sbout the input and sends it to Input to display it all */}
                         </FormControl>
                         <FormMessage/>
                     </FormItem>)}/>
 
                     <FormField control = {form.control} name="country" render = {(field) => (
                     <FormItem>
-                        <FormLabel>Country</FormLabel>
+                        <FormLabel htmlFor="country">Country</FormLabel>
                         <FormControl>
-                            <Input {...field} disabled className = "bg-white"/> {/*form is managed by react-hook-form so field object contains a bunch of info and properties sbout the input and sends it to Input to display it all */}
+                            <Input id="country" {...field}  className = "bg-white"/> {/*form is managed by react-hook-form so field object contains a bunch of info and properties sbout the input and sends it to Input to display it all */}
                         </FormControl>
                     </FormItem>)}/>
 
